2b612ee294777ab4d48be0122914fb4792350335
[dyad] <mskdot6@gmail.com>
2025-08-10 19:49:42 +0530

    [dyad] Fixed "All Countries" radio filter. - wrote 1 file(s)

--- src/redux/services/radioBrowserApi.js
+++ src/redux/services/radioBrowserApi.js
@@ -7,8 +7,22 @@ export const radioBrowserApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         searchStations: builder.query({
-            query: ({ country = '', language = 'english', name = '', hidebroken = true }) => 
-                `/stations/search?countrycode=${country}&language=${language}&name=${name}&limit=500&hidebroken=${hidebroken}&order=clickcount&reverse=true`,
+            query: ({ country = '', language = 'english', name = '', hidebroken = true }) => {
+                const params = new URLSearchParams({
+                    language,
+                    name,
+                    limit: '500',
+                    hidebroken,
+                    order: 'clickcount',
+                    reverse: 'true'
+                });
+
+                if (country) {
+                    params.append('countrycode', country);
+                }
+
+                return `/stations/search?${params.toString()}`;
+            },
         }),
         getCountries: builder.query({
             query: () => '/countries?order=name',
