9a3f1fcd9c513b714cd1a21337b264d9c9e44360
[dyad] <mskdot6@gmail.com>
2025-08-10 19:31:32 +0530

    [dyad] Reduced the logo size - wrote 1 file(s)

--- src/components/Sidebar/Logo.jsx
+++ src/components/Sidebar/Logo.jsx
@@ -3,7 +3,7 @@ import { logo } from "../../assets/images"
 const Logo = () => {
   return (
     <div className="w-full flex flex-row gap-[5px] px-2 lg:justify-start justify-center items-center">
-        <img src={logo} alt="logo" className="w-24"/>
+        <img src={logo} alt="logo" className="w-16"/>
     </div>
   )
 }
