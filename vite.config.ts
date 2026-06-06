4f4a76dacf1d91b31cca3ced9bd5ebcd9f729dfa
[dyad] <mskdot6@gmail.com>
2026-06-06 11:10:05 -0700

    [dyad] Fixed deployment MIME errors by updating path configurations and adding a 404 redirect handler. - wrote 3 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: './',
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,9 @@ export default defineConfig(() => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 }));
